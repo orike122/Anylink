04ab6ae6d3db19db5f03bc02a505723e0eb2e5ae
ori-keidar <orikeidar01@gmail.com>
2019-03-23 18:26:29 +0200

    Add log print

--- Anylink-server/main.py
+++ Anylink-server/main.py
@@ -4,6 +4,7 @@ from server import AnylinkServer
 def main():
     config = Configuration("config.ini")
     server = AnylinkServer(config.bind_address, config = config)
+    print("serving...")
     server.serve_forever()
 
 if __name__ == "__main__":
