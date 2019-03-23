5cb3679e81582b2ffc9d33c08d594a1637294713
ori-keidar <orikeidar01@gmail.com>
2019-03-23 17:14:52 +0200

    Add main

---
+++ Anylink-server/main.py
@@ -0,0 +1,10 @@
+from .conf import Configuration
+from .server import AnylinkServer
+
+def main():
+    config = Configuration("config.ini")
+    server = AnylinkServer(config.bind_address, config = config)
+    server.serve_forever()
+
+if __name__ == "__main__":
+    main()
