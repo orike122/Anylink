a41efff6998c16ccd8cfa128c82f69bd6bdc295b
ori-keidar <orikeidar01@gmail.com>
2019-03-23 16:52:52 +0200

    Add sftp server interface

---
+++ Anylink-server/server_interface.py
@@ -0,0 +1,52 @@
+import paramiko
+import sys
+import os
+import posixpath
+
+class SFTPServerInterface(paramiko.SFTPServerInterface):
+
+    def __init__(self, server,get_user_method):
+        self._base_dir = get_user_method().root_path
+
+    def _local_path(self, sftp_path):
+        if sys.platform == 'win32':
+            #TODO implement for win32
+            raise NotImplementedError("win32 not implemented")
+
+        absolute_base_path = os.path.abspath(self._base_dir)
+        if not sftp_path.startswith("/"):
+            raise ValueError("Invalid SFTP path {path}".format(path=sftp_path))
+
+        #TODO go through this piece of code
+        sp = posixpath.normpath(sftp_path)
+        sp = sp.lstrip("/")
+        assert ('..' not in posixpath.split(sp))
+        lp = os.path.abspath(os.path.join(self._base_dir, sp))
+        assert (os.path.commonprefix((absolute_base_path, lp)) == absolute_base_path)
+        return lp
+
+    def ls_folder(self,sftp_path):
+        local_path = self._local_path(sftp_path)
+        retval = []
+        for fname in os.listdir(local_path):
+            fpath = os.path.join(local_path,fname)
+            retval.append(paramiko.SFTPAttributes.from_stat(os.lstat(fpath),fname))
+        return retval
+
+    def stat(self, sftp_path):
+        local_path = self._local_path(sftp_path)
+        fname = os.path.basename(local_path)
+        return paramiko.SFTPAttributes.from_stat(os.stat(local_path),fname)
+
+    def lstat(self, sftp_path):
+        local_path = self._local_path(sftp_path)
+        fname = os.path.basename(local_path)
+        return paramiko.SFTPAttributes.from_stat(os.lstat(local_path), fname)
+
+    def open(self, sftp_path, flags, attr):
+        local_path = self._local_path(sftp_path)
+        if (flags & os.O_WRONLY) or (flags & os.O_RDWR):
+            return paramiko.SFTP_PERMISSION_DENIED
+        handle = paramiko.SFTPHandle()
+        handle.readfile = open(local_path, "rb")
+        return handle
