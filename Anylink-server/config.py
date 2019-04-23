15c140a574d380c9b6f1522b77ed8f8d0bdad8f4
ori-keidar <orikeidar01@gmail.com>
2019-04-23 16:00:08 +0300

    Create an updated config class

---
+++ Anylink-server/config.py
@@ -0,0 +1,24 @@
+import json
+
+import paramiko
+
+
+class Configuration():
+
+    def __init__(self,json_path,configuration):
+        self.json_path = json_path
+        self.conf = configuration
+        self.load()
+
+    def load(self):
+        with open(self.json_path,"r") as json_file:
+            json_handle = json.load(json_file)
+            listen_port = json_handle[self.conf]["listen_port"]
+            self.bind_addr = ("0.0.0.0",listen_port)
+            host_key_path = json_handle[self.conf]["host_key_path"]
+            self.host_key = paramiko.RSAKey.from_private_key_file(filename=host_key_path)
+            self.database = None
+            #TODO: integrate with database
+
+
+
