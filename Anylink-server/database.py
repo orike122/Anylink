52913ceb18aaabf326847ccad5309d8aca678a10
ori-keidar <orikeidar01@gmail.com>
2019-04-23 16:39:34 +0300

    Add database managing class

---
+++ Anylink-server/database.py
@@ -0,0 +1,44 @@
+import sqlite3
+import hashlib
+
+
+class Database():
+    #magic strings
+    insert = "INSERT INTO ? (email,password_hash,email_hash) VALUES (?,?)"
+    select = "SELECT * FROM ? WHERE email_hash=?"
+
+    def __init__(self, database_path, default_table = None):
+        self.database_path = database_path
+        self._default_table = default_table
+        self.database = sqlite3.connect(self.database_path)
+
+    def set_default_table(self,default_table):
+        self._default_table = default_table
+
+    def add_user(self,username,email,password_hash,table = None):
+        if table is not None and isinstance(table,str):
+            current_table = table
+        else:
+            current_table = self._default_table
+        cursor = self.database.cursor()
+
+        email_hash = hashlib.sha256(email).hexdigest()
+        cursor.execute(self.insert,current_table,(email,password_hash,email_hash))
+        cursor.close()
+        
+    def search_database(self,email_hash,table = None):
+        if table is not None and isinstance(table, str):
+            current_table = table
+        else:
+            current_table = self._default_table
+        cursor = self.database.cursor()
+        cursor.execute(self.select,current_table,email_hash)
+        user = cursor.fetchall()
+        cursor.close()
+        return user
+
+
+
+
+
+
