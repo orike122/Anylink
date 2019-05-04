3ef9cc5f43d7e2ad54eea865f70fee905eac402e
ori-keidar <orikeidar01@gmail.com>
2019-05-04 15:48:15 +0300

    Add default table

--- Anylink-server/add_user.py
+++ Anylink-server/add_user.py
@@ -2,4 +2,4 @@ from config import Configuration
 if __name__ == "__main__":
     config = Configuration("/home/orikeidar01/config.json", "anylink")
     config.database.add_user("testusr@gmail.com",
-                             "$6$/bPljc0H0oOo11nW$eE4vy9BMrHgKvPT//4wiTh7r2YtYTOLrm43qu5h.GyJ1ZrgN2f9lwNrVq6HxSZ6ysjCdIRGgUqXWSW6YSLq4B/")
+                             "$6$/bPljc0H0oOo11nW$eE4vy9BMrHgKvPT//4wiTh7r2YtYTOLrm43qu5h.GyJ1ZrgN2f9lwNrVq6HxSZ6ysjCdIRGgUqXWSW6YSLq4B/","anylink")
