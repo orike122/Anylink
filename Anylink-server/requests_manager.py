b9c31ec3aef727218994ba7ca5d1faa2aa44669d
ori-keidar <orikeidar01@gmail.com>
2019-05-05 00:17:39 +0300

    Add test print

--- Anylink-server/requests_manager.py
+++ Anylink-server/requests_manager.py
@@ -22,6 +22,7 @@ class RequestsManager():
             if not os.path.isfile("/{email_hash}/ssh/authorized_keys".format(email_hash = email_hash)):
                 self.channels[user].send(self.SEND_KEY)
                 data = self.channels[user].recv(56)
+                print(data)
                 if data == self.KEY_SENT:
                     with open("/{email_hash}/storage/key".format(email_hash = email_hash),"r") as f:
                         raw_key = f.read()
