f2638b4c26a2df07b5c8334bb4e8decb3e0cad77
ori-keidar <orikeidar01@gmail.com>
2019-05-04 22:44:36 +0300

    Add creation of ssh keys semi manual for presentation

--- Anylink-Client/main.py
+++ Anylink-Client/main.py
@@ -1,9 +1,25 @@
 from client import Client
-
-
+import readchar
+import json
+import os
 def main():
-    client = Client(("35.237.82.70",8822))
-    client.connect("testusr@gmail.com","abc")
+    with open("config.json","r") as f:
+        json_handle = json.load(f)
+        ip = json_handle["ip"]
+        port = json_handle["port"]
+        if "auth_keys_path" in json_handle:
+            auth_keys_path = json_handle["auth_keys_path"]
+        else:
+            auth_keys_path = None
+
+    if auth_keys_path is None:
+        print("begin ssh-key generation process.........")
+        os.system("ssh-keygen")
+        auth_keys_path = input("insert path of public ssh key: ")
+
+    client = Client((ip,port))
+
+    client.connect("testusr@gmail.com", "abc")
     client.start_client()
 if __name__ == "__main__":
     main()
