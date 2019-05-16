9fdc073e5cbcf49be964dc8e86d21680ccbcfdf9
ori-keidar <orikeidar01@gmail.com>
2019-05-16 18:09:25 +0300

    Change syntax

--- Anylink-web/anylink.py
+++ Anylink-web/anylink.py
@@ -10,7 +10,7 @@ account_manager = None
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if g.user is None:
+        if g.get('user') is None:
             return redirect(url_for('login', next=request.url))
         return f(*args, **kwargs)
     return decorated_function
