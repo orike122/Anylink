278f25fc97f6ad7a3a74993b407cdc03fdcb05b6
ori-keidar <orikeidar01@gmail.com>
2019-05-17 00:02:02 +0300

    Require ssl

--- Anylink-web/anylink.py
+++ Anylink-web/anylink.py
@@ -12,13 +12,6 @@ request_manager = None
 account_manager = None
 
 
-@app.before_request
-def before_request():
-    if request.url.startswith('http://'):
-        url = request.url.replace('http://', 'https://', 1)
-        code = 301
-        return redirect(url, code=code)
-
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
