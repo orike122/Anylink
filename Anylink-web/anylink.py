6f27462d809e6514022b57b7eada91d7d20b2aa2
ori-keidar <orikeidar01@gmail.com>
2019-05-16 23:06:37 +0300

    Add certificate check

--- Anylink-web/anylink.py
+++ Anylink-web/anylink.py
@@ -1,7 +1,7 @@
 from flask import *
 from functools import wraps
 
-app = Flask(__name__,static_url_path='/.well-known')
+app = Flask(__name__,static_url_path='/.well-known/acme-challenge/')
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
