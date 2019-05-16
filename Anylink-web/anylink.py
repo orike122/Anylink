194bd578dce3b72b0e11cb3816d738236b97a5d9
ori-keidar <orikeidar01@gmail.com>
2019-05-16 18:41:23 +0300

    Fix typo

--- Anylink-web/anylink.py
+++ Anylink-web/anylink.py
@@ -11,7 +11,7 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if g.get('user') is None:
-            return redirect(url_for('login', next=request.url))
+            return redirect(url_for('login'))
         return f(*args, **kwargs)
     return decorated_function
 
