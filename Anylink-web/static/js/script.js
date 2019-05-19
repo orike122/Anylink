4d1a7a7b9af57017d89e76b9e75d34bb88526c7d
ori-keidar <orikeidar01@gmail.com>
2019-05-19 15:49:42 +0300

    Fix ajax to download files

--- Anylink-web/static/js/script.js
+++ Anylink-web/static/js/script.js
@@ -9,16 +9,35 @@ function hashpass() {
 }
 
 function send_path(type,name) {
-    $(window).alert('hi');
-    $.ajax({
-      url: "/file_browser",
-      type: "get",
-      data: {jsdata: type+','+name},
-      success: function(response) {
-        $("#file_browser").html(response);
-      },
-      error: function(xhr) {
-        //Do Something to handle error
-      }
-    });
+    if type == 'file'{
+        $.ajax({
+          dataType: 'native',
+          url: "/file_browser",
+          type: "get"
+          xhrFields: {
+            responseType: 'blob'
+          },
+          success: function(blob){
+            console.log(blob.size);
+              var link=document.createElement('a');
+              link.href=window.URL.createObjectURL(blob);
+              link.download=name;
+              document.body.appendChild(link);
+              link.click();
+          }
+        });
+    }
+    else {
+        $.ajax({
+          url: "/file_browser",
+          type: "get",
+          data: {jsdata: type+','+name},
+          success: function(response) {
+            $("#file_browser").html(response);
+          },
+          error: function(xhr) {
+            //Do Something to handle error
+          }
+        });
+    }
 }
