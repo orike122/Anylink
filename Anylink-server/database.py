cdf40b11a423af8267d3ec554d3a85d1f75fcc10
oriko <orikeidar01@gmail.com>
2019-05-28 02:53:23 +0300

    Add docs

--- Anylink-server/database.py
+++ Anylink-server/database.py
@@ -1,21 +1,38 @@
 import sqlite3
 import hashlib
-
+import typing
 
 class Database():
-    #magic strings
+    """Database class, to manage users database"""
+    #----------magic strings----------
     insert = "INSERT INTO {table} (email,password_hash,email_hash) VALUES (?,?,?)"
     select = "SELECT * FROM {table} WHERE email=?"
+    # --------------------------------
 
-    def __init__(self, database_path, default_table = None):
+    def __init__(self, database_path: str, default_table: str = None):
+        """
+        C'tor of Database class
+        :param database_path: Path where the DB is located
+        :param default_table: Optional: Default table for querying
+        """
         self.database_path = database_path
         self._default_table = default_table
         self.database = sqlite3.connect(self.database_path,check_same_thread=False)
 
-    def set_default_table(self,default_table):
+    def set_default_table(self,default_table: str):
+        """
+        Sets the default table
+        :param default_table: Default table for querying
+        """
         self._default_table = default_table
 
-    def add_user(self,email,password_hash,table = None):
+    def add_user(self,email: str,password_hash: str,table: str = None):
+        """
+        Adds new user to DB
+        :param email: user's email
+        :param password_hash: user's password hash
+        :param table: Optional: Default table for querying
+        """
         if table is not None and isinstance(table,str):
             current_table = table
         else:
@@ -27,7 +44,14 @@ class Database():
         cursor.execute(insert,(email,password_hash,email_hash))
         self.database.commit()
         cursor.close()
-    def search_database(self,email,table = None):
+
+    def search_database(self,email: str,table: str = None) -> typing.Dict[str,str]:
+        """
+        Preform a select query to DB
+        :param email: user's email
+        :param table: Optional: Default table for querying
+        :return: Dict of user details of None if user not found
+        """
         if table is not None and isinstance(table, str):
             current_table = table
         else:
