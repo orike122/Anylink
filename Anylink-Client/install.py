259b98f1aec076c38d79dd9633cc372d6125d73a
oriko <orikeidar01@gmail.com>
2019-06-12 21:19:20 +0300

    Add registers setup

--- Anylink-Client/install.py
+++ Anylink-Client/install.py
@@ -1,26 +1,42 @@
 import argparse
 import logging
 import os
-import time
+import winreg
 
 def install_gui(path):
     try:
         os.mkdir(path)
         os.system(r'mv anylinkInit.exe {path}'.format(path=path))
-    except EnvironmentError:
+    except EnvironmentError as e:
         logging.fatal("Error while installin GUI :(")
         raise e
-    return err
 
 def install_client(path):
     try:
         os.system(r'mv client.py {path}'.format(path=path))
         os.system(r'mv main.py {path}'.format(path=path))
+        os.mkdir(os.path.join(path,'ssh'))
     except EnvironmentError as e:
-        logging.fatal("Error while installin GUI :( , exiting.....")
+        logging.fatal("Error while installing client :( ")
         raise e
+
 def init_registers(path):
-    pass
+    reg = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
+    try:
+        winreg.CreateKey(reg, r'SOFTWARE\Anylink')
+    except EnvironmentError as e:
+        logging.fatal("Error while creating Anylink's registry key :(")
+        raise e
+
+    key = winreg.OpenKey(reg, r'SOFTWARE\Anylink')
+    try:
+        winreg.SetValueEx(key,'key_path', 0, winreg.REG_SZ, os.path.join(path,'ssh'))
+        winreg.SetValueEx(key, 'has_init', 0, winreg.REG_DWORD, 0)
+    except EnvironmentError as e:
+        logging.fatal("Error while setting value :( ")
+        raise e
+    finally:
+        winreg.CloseKey(key)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.info())
